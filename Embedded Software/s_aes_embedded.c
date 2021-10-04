ea2ec46b2588fb6e17ec7940e84803fc5b17a225
Barış Berk ZORBA <bbzorba@hotmail.com>
2021-10-04 20:19:41 +0300

    s_aes_lut_embedded

--- Embedded Software/s_aes_embedded.c
+++ Embedded Software/s_aes_embedded.c
@@ -3,7 +3,6 @@
 #define stop_timer()   *((volatile uint32_t*)0xE0001000) = 0x40000000  // Disable CYCCNT register
 #define get_timer()   *((volatile uint32_t*)0xE0001004)               // Get value from CYCCNT register
 
-
 unsigned int in[4] = {0x06,0x0F,0x06,0x0B};
 unsigned int out[4];
 unsigned int RoundKey[3];
@@ -30,8 +29,8 @@ unsigned char RotWords (unsigned char rotating)
 	return y;
 }
 
-//function of MixColumns
-unsigned int MixColumns (unsigned int value)
+//function that includes LUT of multiplication results
+unsigned int MultLUT (unsigned int value)
 {
 		unsigned int ResultofMult[16] =
 		{
@@ -73,71 +72,70 @@ int main()
 	uint32_t beginningcycles=0;
 	uint32_t totalcycles=0;
 
+	//RoundKeys
+	unsigned int RoundKey[3] = {KeyExpansion(0),KeyExpansion(1),KeyExpansion(2)};
+	unsigned int RKey0[4] = {RoundKey[0]>>12, (RoundKey[0]&0x0f00)>>8, (RoundKey[0]&0x00f0)>>4, RoundKey[0]&0x000f};
+	unsigned int RKey1[4] = {RoundKey[1]>>12, (RoundKey[1]&0x0f00)>>8, (RoundKey[1]&0x00f0)>>4, RoundKey[1]&0x000f};
+	unsigned int RKey2[4] = {RoundKey[2]>>12, (RoundKey[2]&0x0f00)>>8, (RoundKey[2]&0x00f0)>>4, RoundKey[2]&0x000f};
+
 	start_timer();
 	beginningcycles = get_timer();
 
-		//RoundKeys
-		unsigned int RoundKey[3] = {KeyExpansion(0),KeyExpansion(1),KeyExpansion(2)};
-		unsigned int RKey0[4] = {RoundKey[0]>>12, (RoundKey[0]&0x0f00)>>8, (RoundKey[0]&0x00f0)>>4, RoundKey[0]&0x000f};
-		unsigned int RKey1[4] = {RoundKey[1]>>12, (RoundKey[1]&0x0f00)>>8, (RoundKey[1]&0x00f0)>>4, RoundKey[1]&0x000f};
-		unsigned int RKey2[4] = {RoundKey[2]>>12, (RoundKey[2]&0x0f00)>>8, (RoundKey[2]&0x00f0)>>4, RoundKey[2]&0x000f};
+	//AddRoundKey1
+	unsigned int state[4] = {in[0] ^ RKey0[0], in[1] ^ RKey0[1], in[2] ^ RKey0[2], in[3] ^ RKey0[3]};
 
-		//AddRoundKey1
-		unsigned int state[4] = {in[0] ^ RKey0[0], in[1] ^ RKey0[1], in[2] ^ RKey0[2], in[3] ^ RKey0[3]};
+	//SubNibble1
+	state[0] = getSBoxValue(state[0]);
+	state[1] = getSBoxValue(state[1]);
+	state[2] = getSBoxValue(state[2]);
+	state[3] = getSBoxValue(state[3]);
 
-		//SubNibble1
-		state[0] = getSBoxValue(state[0]);
-		state[1] = getSBoxValue(state[1]);
-		state[2] = getSBoxValue(state[2]);
-		state[3] = getSBoxValue(state[3]);
+	//ShiftRows1
+	unsigned int temp1;
+	temp1 = state[1];
+	state[1] = state[3];
+	state[3] = temp1;
 
-		//ShiftRows1
-		unsigned int temp1;
-		temp1 = state[1];
-		state[1] = state[3];
-		state[3] = temp1;
+	unsigned int tempstate0 = state[0];
+	unsigned int tempstate1 = state[1];
+	unsigned int tempstate2 = state[2];
+	unsigned int tempstate3 = state[3];
 
-		unsigned int tempstate0 = state[0];
-		unsigned int tempstate1 = state[1];
-		unsigned int tempstate2 = state[2];
-		unsigned int tempstate3 = state[3];
+	//MixColumns
+	state[0] = tempstate0 ^ (MultLUT(tempstate1));
+	state[1] = (MultLUT(tempstate0)) ^ tempstate1;
+	state[2] = tempstate2 ^ (MultLUT(tempstate3));
+	state[3] = (MultLUT(tempstate2)) ^ tempstate3;
 
-		//MixColumns
+	//AddRoundKey2
+	state[0] = state[0] ^ RKey1[0];
+	state[1] = state[1] ^ RKey1[1];
+	state[2] = state[2] ^ RKey1[2];
+	state[3] = state[3] ^ RKey1[3];
 
-		state[0] = tempstate0 ^ (MixColumns(tempstate1));
-		state[1] = (MixColumns(tempstate0)) ^ tempstate1;
-		state[2] = tempstate2 ^ (MixColumns(tempstate3));
-		state[3] = (MixColumns(tempstate2)) ^ tempstate3;
+	//SubNibble2
+	state[0] = getSBoxValue(state[0]);
+	state[1] = getSBoxValue(state[1]);
+	state[2] = getSBoxValue(state[2]);
+	state[3] = getSBoxValue(state[3]);
 
-		//AddRoundKey2
-		state[0] = state[0] ^ RKey1[0];
-		state[1] = state[1] ^ RKey1[1];
-		state[2] = state[2] ^ RKey1[2];
-		state[3] = state[3] ^ RKey1[3];
+	//ShiftRows2
+	unsigned int temp2;
+	temp2 = state[1];
+	state[1] = state[3];
+	state[3] = temp2;
 
-		//SubNibble2
-		state[0] = getSBoxValue(state[0]);
-		state[1] = getSBoxValue(state[1]);
-		state[2] = getSBoxValue(state[2]);
-		state[3] = getSBoxValue(state[3]);
+	//AddRoundKey3
+	state[0] = state[0] ^ RKey2[0];
+	state[1] = state[1] ^ RKey2[1];
+	state[2] = state[2] ^ RKey2[2];
+	state[3] = state[3] ^ RKey2[3];
 
-		//ShiftRows2
-		unsigned int temp2;
-		temp2 = state[1];
-		state[1] = state[3];
-		state[3] = temp2;
-
-		//AddRoundKey3
-		state[0] = state[0] ^ RKey2[0];
-		state[1] = state[1] ^ RKey2[1];
-		state[2] = state[2] ^ RKey2[2];
-		state[3] = state[3] ^ RKey2[3];
-
-		//Ciphertext
-		out[0] = state[0];
-		out[1] = state[1];
-		out[2] = state[2];
-		out[3] = state[3];
+	//Ciphertext
+	out[0] = state[0];
+	out[1] = state[1];
+	out[2] = state[2];
+	out[3] = state[3];
 
 	totalcycles = get_timer() - beginningcycles;
 	stop_timer();
